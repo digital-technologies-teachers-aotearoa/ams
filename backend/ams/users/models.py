61430f1543c3cfdd82d35c9e333cc57ac93a2501
Andrew Caudwell <andrewc@catalyst.net.nz>
2023-07-11 12:30:59 +1200

    RM#97726: Add user and membership status to navigation bar
    Move navigation bar into to base.html

--- backend/ams/users/models.py
+++ backend/ams/users/models.py
@@ -1,3 +1,5 @@
+from typing import Any, Tuple
+
 from django.contrib.auth.models import User
 from django.db.models import (
     CASCADE,
@@ -29,3 +31,36 @@ class UserMembership(Model):
     membership_option = ForeignKey(MembershipOption, on_delete=CASCADE, related_name="user_memberships")
     created_datetime = DateTimeField()
     approved_datetime = DateTimeField(null=True)
+
+
+class UserMemberStatus(TextChoices):
+    NONE = "NONE", _("None")
+    PENDING = "PENDING", _("Pending")
+    APPROVED = "APPROVED", _("Approved")
+
+
+class UserMemberInfo:
+    def __init__(self, user: User) -> None:
+        self.user_membership = UserMembership.objects.filter(user=user).order_by("-created_datetime").first()
+
+    def status(self) -> Tuple[str, Any]:
+        if not self.user_membership:
+            return UserMemberStatus.NONE
+
+        if self.user_membership.approved_datetime is None:
+            return UserMemberStatus.PENDING
+
+        return UserMemberStatus.APPROVED
+
+
+def user_member_info(user: User) -> UserMemberInfo:
+    if hasattr(user, "_member_info"):
+        member_info: UserMemberInfo = user._member_info
+        return member_info
+
+    # Cache against user object
+    user._member_info = UserMemberInfo(user)
+    return user._member_info
+
+
+User.member = property(lambda self: user_member_info(self))
---
+++ backend/ams/users/tests/test_user_status.py
@@ -0,0 +1,133 @@
+from django.contrib.sites.requests import RequestSite
+from django.test import RequestFactory, TestCase
+from django.utils import timezone
+from registration.models import RegistrationProfile
+
+from ..models import MembershipOption, MembershipOptionType, UserMembership
+
+
+class UserStatusTests(TestCase):
+    def setUp(self) -> None:
+        request = RequestFactory().get("/")
+
+        self.user = RegistrationProfile.objects.create_inactive_user(
+            RequestSite(request),
+            username="user@example.com",
+            email="user@example.com",
+            first_name="John",
+            last_name="Smith",
+            password="valid password",
+        )
+
+        self.user.is_active = True
+        self.user.save()
+
+        membership_option = MembershipOption.objects.create(
+            name="Membership Option", type=MembershipOptionType.INDIVIDUAL, duration="P1M", cost="1.00"
+        )
+
+        self.user_membership = UserMembership.objects.create(
+            user=self.user,
+            membership_option=membership_option,
+            created_datetime=timezone.now(),
+            approved_datetime=None,
+        )
+
+    def test_get_login_screen(self) -> None:
+        # When
+        response = self.client.get("/users/login/")
+
+        # Then
+        self.assertTemplateUsed(response, "registration/login.html")
+
+    def test_should_login_successfully_with_valid_details(self) -> None:
+        # When
+        response = self.client.post(
+            "/users/login/",
+            {
+                "username": self.user.username,
+                "password": "valid password",
+            },
+        )
+
+        # Then
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(response.url, "/")
+
+    def test_should_redirect_to_next_url_on_successful_login(self) -> None:
+        # When
+        response = self.client.post(
+            "/users/login/",
+            {"username": self.user.username, "password": "valid password", "next": "/membership/"},
+        )
+
+        # Then
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(response.url, "/membership/")
+
+    def test_should_show_error_with_invalid_details(self) -> None:
+        # When
+        response = self.client.post(
+            "/users/login/",
+            {
+                "username": self.user.username,
+                "password": "invalid password",
+            },
+        )
+
+        # Then
+        self.assertContains(response, "Invalid email or password")
+
+    def test_should_show_logged_in_users_first_and_last_name(self) -> None:
+        # Given
+        self.client.force_login(self.user)
+
+        # When
+        response = self.client.get("/")
+
+        # Then
+        self.assertContains(response, f"You are logged in as {self.user.first_name} {self.user.last_name}")
+
+    def test_should_show_logged_in_nameless_users_username(self) -> None:
+        # Given
+        self.user.first_name = ""
+        self.user.last_name = ""
+        self.user.save()
+
+        self.client.force_login(self.user)
+
+        # When
+        response = self.client.get("/")
+
+        # Then
+        self.assertContains(response, f"You are logged in as {self.user.username}")
+
+    def test_should_not_show_logged_out_users_name(self) -> None:
+        # When
+        response = self.client.get("/")
+
+        # Then
+        self.assertNotContains(response, "You are logged in as")
+
+    def test_should_show_logged_in_users_pending_membership_status(self) -> None:
+        # Given
+        self.client.force_login(self.user)
+
+        # When
+        response = self.client.get("/")
+
+        # Then
+        self.assertContains(response, "Your membership is Pending")
+
+    def test_should_not_show_logged_in_users_approved_membership_status(self) -> None:
+        # Given
+        self.user_membership.approved_datetime = timezone.now()
+        self.user_membership.save()
+
+        self.client.force_login(self.user)
+
+        # When
+        response = self.client.get("/")
+
+        # Then
+        self.assertNotContains(response, "Your membership is")
