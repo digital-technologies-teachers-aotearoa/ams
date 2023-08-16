1c9dd0ad79f6f2b27965752a235b0f1bdb58ce8d
Andrew Caudwell <andrewc@catalyst.net.nz>
2023-08-16 13:58:36 +1200

    RM#98023: Add place holder footer
    Add javascript from theme

--- update-theme.bash
+++ update-theme.bash
@@ -3,5 +3,6 @@ set -e
 
 cp ./frontend/css/main.min.css ./backend/ams/base/static/css/base.min.css
 cp ./frontend/css/main.css ./backend/ams/base/static/css/base.css
+cp -r ./frontend/js ./backend/ams/base/static/
 cp -r ./frontend/fonts ./backend/ams/base/static/
 cp -r ./frontend/images ./backend/ams/dtta/static/
