e9bba48c9e7b0cd6297ca0d5ac024a91f81433dd
Andrew Caudwell <andrewc@catalyst.net.nz>
2023-08-18 15:07:17 +1200

    RM#98023: Main menu using wagtailmenus with theme
    Add generic page type
    
    Populate menu

--- backend/ams/base/models.py
+++ backend/ams/base/models.py
@@ -5,6 +5,7 @@ from wagtail.models import Page
 
 
 class HomePage(Page):
+    is_creatable = False
     heading = CharField(blank=True, max_length=255, default="Home")
     subheading = CharField(blank=True, max_length=255, default="")
     body = RichTextField(blank=True, default="Welcome to the homepage")
@@ -17,6 +18,7 @@ class HomePage(Page):
 
 
 class MembershipPage(Page):
+    is_creatable = False
     heading = CharField(blank=True, max_length=255, default="Membership")
     body = RichTextField(
         blank=True, default="<p>You can sign up either individually or for your organisation.  Please select:</p>"
@@ -26,7 +28,17 @@ class MembershipPage(Page):
 
 
 class EmailConfirmationPage(Page):
+    is_creatable = False
     heading = CharField(blank=True, max_length=255, default="Email Confirmation Successful!")
     body = RichTextField(blank=True, default="Congratulations! Your email has been successfully confirmed.")
+
+    content_panels = Page.content_panels + [FieldPanel("heading"), FieldPanel("body")]
+
+
+class GenericPage(Page):
+    is_creatable = True
+
+    heading = CharField(blank=True, max_length=255, default="Heading")
+    body = RichTextField(blank=True, default="")
 
     content_panels = Page.content_panels + [FieldPanel("heading"), FieldPanel("body")]
