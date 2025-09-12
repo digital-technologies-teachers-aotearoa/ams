0eac00d2e9b48e2a0024047b288488c07dbc915b
Jack Morgan <jackmorgannz@gmail.com>
2025-09-12 01:22:41 +0000

    Improve dev experience with util commands

---
+++ ams/utils/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class UtilsConfig(AppConfig):
+    name = "ams.utils"
+    verbose_name = _("Utils")
+    default_auto_field = "django.db.models.BigAutoField"
