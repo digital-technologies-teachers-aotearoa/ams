77714cf6c1cf421ac13fb34212d80755ebdb0b3e
Jack Morgan <jackmorgannz@gmail.com>
2025-11-23 12:22:04 +1300

    Create CMS block for columns
    Fixes #82

---
+++ ams/cms/blocks/__init__.py
@@ -0,0 +1,15 @@
+from .captioned_image_block import CaptionedImageBlock
+from .columns_block import ColumnsBlock
+from .content_stream_blocks import ColumnContentStreamBlocks
+from .content_stream_blocks import ContentStreamBlocks
+from .heading_block import HeadingBlock
+from .layout_stream_blocks import ContentAndLayoutStreamBlocks
+
+__all__ = [
+    "CaptionedImageBlock",
+    "ColumnContentStreamBlocks",
+    "ColumnsBlock",
+    "ContentAndLayoutStreamBlocks",
+    "ContentStreamBlocks",
+    "HeadingBlock",
+]
