699a1eb31425b59fd1a10725080bb72c215dc4d4
Jack Morgan <jackmorgannz@gmail.com>
2025-12-15 13:37:35 +1300

    Support ngrok within allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,6 +14,9 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+NGROK_HOST = env("NGROK_HOST", default=None)
+if NGROK_HOST:
+    ALLOWED_HOSTS.append(NGROK_HOST)
 
 # CACHES
 # ------------------------------------------------------------------------------
