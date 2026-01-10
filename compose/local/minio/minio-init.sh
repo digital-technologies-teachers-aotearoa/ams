a3225df8183efcad6fa57e531b8a78c9c47ae4ef
Jack Morgan <jackmorgannz@gmail.com>
2026-01-11 12:19:51 +1300

    chore: Remove bucket policies as ACLs are used

--- compose/local/minio/minio-init.sh
+++ compose/local/minio/minio-init.sh
@@ -5,19 +5,15 @@ set -e
 # ---------------------------
 # This script configures the local MinIO server with a single bucket named 'ams-media'.
 # The bucket contains two directories:
-#   1. public/  - Allows anonymous (unauthenticated) read access. Intended for files that should be accessible to anyone.
-#   2. private/ - Requires authentication for all access. Intended for sensitive or internal files.
+#   1. public/
+#   2. private/
 #
-# The script uses the MinIO Client (mc) to connect to the MinIO server and set up the bucket and policies.
+# The script uses the MinIO Client (mc) to connect to the MinIO server and set up the bucket.
 #
 # Usage:
 #   This script is intended to be run automatically by Docker Compose as part of the local development environment.
 #   It assumes the MinIO server is available at http://minio:9000 and that the MINIO_ROOT_USER and MINIO_ROOT_PASSWORD
 #   environment variables are set.
-#
-# Bucket Policies:
-#   - ams-media/public/*: Anonymous users can download (read) files. Upload and delete require authentication.
-#   - ams-media/private/*: All operations require authentication.
 #
 # Note: The script waits briefly to ensure the MinIO server is ready before running commands.
 
@@ -30,7 +26,3 @@ mc alias set local http://minio:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 # Create ams-media buckets (if they doesn't exist)
 mc mb --ignore-existing local/ams-media-public
 mc mb --ignore-existing local/ams-media-private
-
-# Set bucket policies
-mc anonymous set-json /public-bucket-policy.json local/ams-media-public
-mc anonymous set-json /private-bucket-policy.json local/ams-media-private
